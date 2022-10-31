8cce3cb8c67d0e170d7c7fc8d23dc1104e638972
Constantine <kosta.tovstik@gmail.com>
2022-10-31 13:49:21 +0200

    initial commit

---
+++ src/main/kotlin/com/sirkostya009/campusmaps/CampusMapsApplication.kt
@@ -0,0 +1,11 @@
+package com.sirkostya009.campusmaps
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CampusMapsApplication
+
+fun main(args: Array<String>) {
+    runApplication<CampusMapsApplication>(*args)
+}
---
+++ src/test/kotlin/com/sirkostya009/campusmaps/CampusMapsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sirkostya009.campusmaps
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CampusMapsApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
