d8e1886987fff8e8f4b092daad97b16c55ea8106
Constantine <kosta.tovstik@gmail.com>
2022-10-31 20:26:35 +0200

    added a alternativeGetMap method to MapApi

--- src/main/kotlin/com/sirkostya009/campusmaps/api/MapApi.kt
+++ src/main/kotlin/com/sirkostya009/campusmaps/api/MapApi.kt
@@ -2,10 +2,7 @@ package com.sirkostya009.campusmaps.api
 
 import com.sirkostya009.campusmaps.Constants
 import com.sirkostya009.campusmaps.service.MapService
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/v1/")
@@ -16,6 +13,11 @@ class MapApi(
     @GetMapping("/map", produces = [Constants.MapFormat])
     fun getMap(@RequestParam(name = "building", required = true) buildingNo: Int,
                @RequestParam(name = "floor", required = true) floorLevel: Int) =
+        service.getMap(buildingNo, floorLevel)
+
+    @GetMapping("/map/{buildingNo}/{floorLevel}", produces = [Constants.MapFormat])
+    fun alternativeGetMap(@PathVariable buildingNo: Int,
+                          @PathVariable floorLevel: Int) =
         service.getMap(buildingNo, floorLevel)
 
 }
